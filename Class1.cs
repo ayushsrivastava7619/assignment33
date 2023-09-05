87d0e8f82873eeeca6dca9b7fcbc776250e881db
ayushsrivastava7619 <shivamsrivastava23112001@gmail.com>
2023-09-05 20:44:27 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment33
+{
+    internal class internalclass
+    {
+        internal int x;
+        internal void method()
+        {
+            Console.WriteLine("internal class is accessed");
+            Console.WriteLine("internal class variable have value: " + x);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment33
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            internalclass inti = new internalclass();
+            inti.x = 45;
+
+            Console.WriteLine("Main class got access the other internal class");
+            inti.method();
+            Console.Read();
+        }
+    }
+}
